59c1ec8384d79b9676caaa1d5b4f185454e76a7f
Akhror Ziyodinov <ahror.ziyodinov@gmail.com>
2024-09-10 15:09:40 +0200

    initial commit

---
+++ Resunet/DAL/DbHelper.cs
@@ -0,0 +1,7 @@
+﻿namespace Resunet.DAL
+{
+	public class DbHelper
+	{
+		public static string ConnectionString = "Server=127.0.0.1;Port=5432;Database=resunet;User Id=postgres;Password=password1697;";
+	}
+}
